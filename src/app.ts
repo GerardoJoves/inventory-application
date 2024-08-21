16e46f35ad022f6b18501579b27b535f3ab39e50
Gerardo Joves <gerardooenrrique@gmail.com>
2024-08-20 23:10:27 -0400

    set up file structure

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
+import express from 'express';
+import path from 'node:path';
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Settings
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+// Middleware
+app.use(express.static(path.join(import.meta.dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`App running on port ${PORT}`));
