fb78e04885e83e0abe5fb9411badd97c8d835b3f
Gerardo Joves <gerardooenrrique@gmail.com>
2024-09-09 14:03:07 -0400

    update populatedb script

--- src/db/populatedb.ts
+++ src/db/populatedb.ts
@@ -1,5 +1,6 @@
 #! /usr/bin/env node
-
+import fs from 'node:fs';
+import path from 'node:path';
 import pg from 'pg';
 import dotenv from 'dotenv';
 const Client = pg.Client;
@@ -17,7 +18,6 @@ interface Game {
   title: string;
   description: string;
   release_date: string;
-  price: number;
   genres: string[];
   developers: string[];
 }
@@ -30,52 +30,17 @@ interface GamesByDeveloper {
   [key: string]: number[];
 }
 
-const games = [
-  {
-    title: 'The Last of Us Part I',
-    description:
-      'Set in the post-apocalyptic United States, the game tells the story of survivors Joel and Ellie as they work together to survive their westward journey across what remains of the country to find a possible cure for the modern fungal plague that has nearly decimated the entire human race.',
-    release_date: '2022-09-01',
-    price: 69.99,
-    genres: ['Adventure', 'Shooter'],
-    developers: ['Naughty Dog LLC', 'Iron Galaxy Studios'],
-  },
-  {
-    title: 'Life is Strange',
-    description:
-      'Life is Strange is a five part episodic game that sets out to revolutionize story based choice and consequence games by allowing the player to rewind time and affect the past, present and future.',
-    release_date: '2015-01-29',
-    price: 15.99,
-    genres: ['Adventure', 'RPG'],
-    developers: ["DON'T NOD", 'Square Enix'],
-  },
-  {
-    title: "Uncharted 3: Drake's Deception",
-    description:
-      'A search for the fabled "Atlantis of the Sands" propels fortune hunter Nathan Drake on a trek into the heart of the Arabian Desert. When the terrible secrets of this lost city are unearthed, Drake\'s quest descends into a desperate bid for survival that strains the limits of his endurance and forces him to confront his deepest fears.',
-    release_date: '2011-11-01',
-    price: 8.99,
-    genres: ['Adventure', 'Platform', 'Shooter'],
-    developers: ['Naughty Dog', 'Sony Computer Entertainment'],
-  },
-  {
-    title: 'Outlast',
-    description:
-      "Hell is an experiment you can't survive in Outlast, a first-person survival horror game developed by veterans of some of the biggest game franchises in history. As investigative journalist Miles Upshur, explore Mount Massive Asylum and try to survive long enough to discover its terrible secret... if you dare.",
-    release_date: '2013-09-04',
-    price: 10.49,
-    genres: ['Adventure', 'Indie', 'Survival Horror'],
-    developers: ['Red Barrels'],
-  },
-];
+const dataPath = path.join(import.meta.dirname, 'archive/games.json');
+const data = fs.readFileSync(dataPath, 'utf8');
+const games = JSON.parse(data) as Game[];
 
 const gamesByGenre: GamesByGenre = {};
 const gamesByDeveloper: GamesByDeveloper = {};
 
 async function insertGame(game: Game) {
   const SQL =
-    'INSERT INTO games (title, description, release_date, price) VALUES ($1, $2, $3, $4) RETURNING id;';
-  const values = [game.title, game.description, game.release_date, game.price];
+    'INSERT INTO games (title, description, release_date) VALUES ($1, $2, $3) RETURNING id;';
+  const values = [game.title, game.description, game.release_date];
   const { rows } = await client.query<{ id: number }>(SQL, values);
   const gameId = rows[0].id;
   game.genres.forEach((genre) => {
@@ -121,8 +86,8 @@ const createTablesSQL = `
 CREATE TABLE IF NOT EXISTS games (
   id SERIAL PRIMARY KEY,
   title VARCHAR(100) NOT NULL,
-  description VARCHAR(1000) NOT NULL CHECK (LENGTH(name) >= 10),
-  release_date DATE,
+  description VARCHAR(2000) NOT NULL CHECK (LENGTH(description) >= 10),
+  release_date DATE
 );
 
 CREATE TABLE IF NOT EXISTS genres (
