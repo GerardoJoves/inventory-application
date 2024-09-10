ff4f06a10fad334027400f333431efa930d48038
Gerardo Joves <gerardooenrrique@gmail.com>
2024-09-09 21:44:25 -0400

    add pg connection config ssl: true

--- dist/db/pool.js
+++ dist/db/pool.js
@@ -8,5 +8,6 @@ const connection = {
     host: process.env.PG_HOST,
     port: process.env.PG_PORT ? parseInt(process.env.PG_PORT) : 5432,
     database: process.env.PG_DATABASE,
+    ssl: true,
 };
 export default new Pool(connection);
